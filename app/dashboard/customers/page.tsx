7375efd6dfa5afc15598731faa8abfcb37f3cfde
Reno Angelo Banderlipe <personal.renoangelo@gmail.com>
2024-11-22 17:55:13 +0800

    updated invoice with authentication

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Customer | Acme Dashboard',
+};
 export default function Page() {
     return <p>Customer Page</p>;
 }
